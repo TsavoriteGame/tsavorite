f5b218d93ac087c2c017d8444bcf2eedb104ed32
Kyle Kemp <kyle@seiyria.com>
2022-09-06 21:00:05 -0500

    fix lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { ElectronService } from './core/services';
 import { TranslateService } from '@ngx-translate/core';
-import { APP_CONFIG } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-export const APP_CONFIG = {
+export const config = {
   production: false,
   environment: 'LOCAL'
 };
