d9d01c81abfa1d916fde3aa0ef7bf2bd15c1c198
Kyle Kemp <kyle@seiyria.com>
2022-09-06 21:05:54 -0500

    Add unbreakable key test

--- content/reactions/Unlocks.spec.ts
+++ content/reactions/Unlocks.spec.ts
@@ -29,6 +29,40 @@ test('A level 2 key should unlock a level 1 lock', () => {
   });
 
   expect(result.success).toBe(true);
+  expect(result.consumeSource).toBe(true);
+  expect(result.consumeTarget).toBe(true);
+
+});
+
+test('An unbreakable level 2 key should unlock a level 1 lock and NOT break', () => {
+
+  const key: ItemConfig = {
+    name: 'Level 2 Key',
+    parts: [{ name: 'Orichalcum', descriptors: { [Descriptor.Unbreakable]: { level: 1 } } }],
+    interaction: { name: Interaction.Unlocks, level: 2 }
+  };
+
+  const lock: ItemConfig = {
+    name: 'Level 1 Lock',
+    parts: [
+      {
+        name: 'Lock Mechanism',
+        descriptors: {
+          [Descriptor.Locked]: { level: 1 }
+        }
+      }
+    ]
+  };
+
+  const result = UnlockApplications[Descriptor.Locked]({
+    sourceAction: Interaction.Unlocks,
+    sourceItem: key,
+    targetItem: lock
+  });
+
+  expect(result.success).toBe(true);
+  expect(result.consumeSource).toBe(false);
+  expect(result.consumeTarget).toBe(true);
 
 });
 
@@ -59,5 +93,7 @@ test('A level 1 key should not unlock a level 2 lock', () => {
   });
 
   expect(result.success).toBe(false);
+  expect(result.consumeSource).toBe(false);
+  expect(result.consumeTarget).toBe(false);
 
 });
