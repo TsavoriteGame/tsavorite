2148defbecc12bf90a5d04863efe0c19475eb054
Kyle Kemp <kyle@seiyria.com>
2022-09-14 15:56:41 -0500

    fix items

--- scripts/items.js
+++ scripts/items.js
@@ -5,6 +5,54 @@ const yaml = require('js-yaml');
 
 const allItems = [];
 
+const validateItems = () => {
+  allItems.forEach(item => {
+    if (!item.name) {
+      throw new Error(`Item ${JSON.stringify(item)} is missing a name`);
+    }
+
+    if(item.parts.length === 0) {
+      throw new Error(`Item ${item.name} is missing parts`);
+    }
+
+    item.parts.forEach(part => {
+      if(!part.name) {
+        throw new Error(`Item ${item.name} is missing a part name`);
+      }
+
+      if(!part.primaryDescriptor) {
+        throw new Error(`Item ${item.name} is missing a primary descriptor`);
+      }
+
+      const allDescriptorKeys = Object.keys(part.descriptors || {});
+      if(allDescriptorKeys.length === 0) {
+        throw new Error(`Item ${item.name} is missing descriptors`);
+      }
+
+      allDescriptorKeys.forEach(key => {
+        const descriptor = part.descriptors[key];
+        if(!descriptor) {
+          throw new Error(`Item ${item.name} is missing a descriptor for ${key}`);
+        }
+
+        if(!descriptor.level) {
+          throw new Error(`Item ${item.name} is missing a level for ${key}`);
+        }
+      })
+    });
+
+    if(item.interaction) {
+      if(!item.interaction.name) {
+        throw new Error(`Item ${item.name} is missing an interaction name`);
+      }
+
+      if(!item.interaction.level) {
+        throw new Error(`Item ${item.name} is missing an interaction level`);
+      }
+    }
+  });
+};
+
 const loadItems = async () => {
   const files = await readdir('content/items', ['*.json']);
   files.forEach(file => {
@@ -12,6 +60,7 @@ const loadItems = async () => {
     allItems.push(...data);
   });
 
+  validateItems();
   fs.writeFileSync('content/items/items.json', JSON.stringify(allItems));
 };
 
