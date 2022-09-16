0d44d7c5b5f91ecd7b6cba416637d0446addfee9
jamspoon <jamspoonart@gmail.com>
2022-09-16 17:33:04 -0500

    implement acid dilution
    Wets -> Container with Corrosive part now causes the Corrosive part to lose a Corrosive level

---
+++ content/middleware/dilute-corrosive.ts
@@ -0,0 +1,40 @@
+import { decreaseDescriptorLevelForPart, getDescriptorLevel, getPartWithDescriptor } from '../helpers';
+import { Descriptor, Interaction, ItemConfig, MiddlewareType, PostMiddleware, ReactionExtendedArgs, ReactionResponse } from '../interfaces';
+
+export function shouldDilute (item: ItemConfig) {
+  return undefined != getPartWithDescriptor(item, Descriptor.Corrosive) && 0 < getDescriptorLevel(item, Descriptor.Container);
+}
+
+/**
+ * We check if the target item has any corrosive element, and if so dilute that corrosive
+ */
+export class DiluteCorrosive implements PostMiddleware {
+    
+  triggers: MiddlewareType [] = ['post'];
+
+  isEnabled () { return true; }
+
+  shouldPostFire (args: ReactionExtendedArgs, response: ReactionResponse) {
+    return args.sourceItem.interaction
+        && args.sourceItem.interaction.name === Interaction.Wets
+        && response.newTarget
+        && shouldDilute(response.newTarget);
+  }
+
+  shouldPostBlock () { return false; }
+
+  post (args: ReactionExtendedArgs, response: ReactionResponse) {
+
+    const part = getPartWithDescriptor(args.targetItem, Descriptor.Corrosive);
+    decreaseDescriptorLevelForPart(part, Descriptor.Corrosive, 1);
+
+    if (response.success)
+      response.message = `${response.message} Acid diluted!`;
+    else
+      response.message = `Acid diluted!`;
+
+    response.success = true;
+
+    return response;
+  }
+}
