9feea873592f5872e2653982c422758a83859507
jamspoon <jamspoonart@gmail.com>
2022-09-21 16:10:12 -0500

    fix recipe validation

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -6,9 +6,21 @@ const { sortBy } = require('lodash');
 
 const allRecipes = [];
 
+const validateIngredients = (recipe) => {
+  if (!recipe.ingredients) return false;
+  let valid = true;
+  recipe.ingredients.forEach(ingredient => {
+    if (!valid) return;
+    if (!ingredient.name || !ingredient.descriptor || !ingredient.level) valid = false;
+  });
+
+  return valid;
+}
+
 const validateRecipes = () => {
   allRecipes.forEach(item => {
-    if (!item.name) {
+    if (!item.name || !item.icon || !item.descriptor
+     || !item.interaction || !validateIngredients(item)) {
       throw new Error(`Recipe ${JSON.stringify(item)} is missing a name`);
     }
   });
