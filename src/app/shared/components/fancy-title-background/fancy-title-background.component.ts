ad537d4cf40d0b9029d7aedff6061f17b62c78c2
Kyle Kemp <kyle@seiyria.com>
2022-09-22 00:33:35 -0500

    fix build

---
+++ src/app/shared/components/fancy-title-background/fancy-title-background.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fancy-title-background',
+  templateUrl: './fancy-title-background.component.html',
+  styleUrls: ['./fancy-title-background.component.scss']
+})
+export class FancyTitleBackgroundComponent implements OnInit {
+
+  @Input() text: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
