eba53ffe06409c32bcdf9fb2a61fe43d3520149e
Kyle Kemp <kyle@seiyria.com>
2022-09-29 22:06:23 -0500

    fix lint

--- src/app/gameplay-backpack/gameplay-backpack.component.ts
+++ src/app/gameplay-backpack/gameplay-backpack.component.ts
@@ -4,7 +4,8 @@ import { Observable, Subject, Subscription, timer } from 'rxjs';
 import { getCombinationBetweenTwoItems,
   getReactionBetweenTwoItems } from '../../../content/helpers';
 import { IItemConfig } from '../../../content/interfaces';
-import { AddBackpackItem, ReduceHealth, RemoveBackpackItem, SetBackpackItemLockById, UpdateBackpackItem } from '../core/services/game/actions';
+import { AddBackpackItem, ReduceHealth, RemoveBackpackItem,
+  SetBackpackItemLockById, UpdateBackpackItem } from '../core/services/game/actions';
 import { ContentService } from '../core/services/game/content.service';
 import { GameConstant, GameService } from '../core/services/game/game.service';
 import { GameState, IGameCharacter } from '../core/services/game/stores';
